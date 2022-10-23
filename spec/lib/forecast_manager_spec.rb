a7762719659db3055ccf7e6b2d83903c5ef61f54
SergSergiienko <serg.sergiienko@gmail.com>
2022-10-23 19:29:31 -0400

    fix forecast manager spec

--- spec/lib/forecast_manager_spec.rb
+++ spec/lib/forecast_manager_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
+
 RSpec.describe ForecastManager do
+  subject { ForecastManager }
+
   it { is_expected.to respond_to :get_data_with }
   it { is_expected.to respond_to :modifiers_chain }
+
+  context '#get_data_with' do
+    let!(:modifier_instance) { double('SomeModifierClass') }
+
+    before { allow(ForecastManager).to receive(:modifiers_chain).and_return [modifier_instance] }
+    after { ForecastManager.get_data_with('test', 'test') }
+
+    it { expect(modifier_instance).to receive(:handle) }
+  end
 end
